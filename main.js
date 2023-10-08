0320f8d18be8f722ca3716b9dbaeb62e5ae29de0
Udhbhav <21889udbhav@gmail.com>
2023-10-08 23:18:15 +0530

    Add funcitonality of creating line

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let c = document.getElementById("myCanvas");
 let ctx = c.getContext("2d");
+let isMouseDown = false;
+let startY = 400;
 
 function createFirstPlatform() {
     ctx.fillStyle = "black";
@@ -13,6 +15,7 @@ function createPlatform() {
     let x = Math.random() * (canvasWidth - 50); 
     ctx.fillRect(x, 400, 50, 200);
 }
+
 function playerSpawn() {
     ctx.fillStyle = "blue";
     let canvasWidht = c.widht;
@@ -20,9 +23,35 @@ function playerSpawn() {
     ctx.fillRect(5, 360, 40, 40);
 }
 
+window.addEventListener('mousedown', () => {
+        isMouseDown = true;
+        ctx.strokeStyle = "blue";
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(40, 380); // Start point
+        ctx.lineTo(40, startY); // Initial lineTo position
+        ctx.stroke();
+
+        // Start a continuous drawing loop
+        drawInterval = setInterval(() => {
+            if (isMouseDown) {
+                // Continuously increase the lineTo position
+                startY -= 1; // You can adjust the speed by changing the increment
+                ctx.lineTo(40, startY);
+                ctx.stroke();
+            }
+        }, 10); // Adjust the interval for the desired drawing speed
+    });
+
+window.addEventListener('mouseup', () => {
+    isMouseDown = false;
+
+    // Stop the continuous drawing loop
+    clearInterval(drawInterval);
+});
+
 createFirstPlatform();
 playerSpawn();
 createPlatform();
 createPlatform();
 createPlatform();
-
