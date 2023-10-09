66dac128801414d7499c08123cc4388b91860b86
Udhbhav <21889udbhav@gmail.com>
2023-10-09 17:31:37 +0530

    Add x axis funcitonality

--- main.js
+++ main.js
@@ -2,34 +2,42 @@ let c = document.getElementById("myCanvas");
 let ctx = c.getContext("2d");
 let isMouseDown = false;
 let startY = 400;
+let endY = 400;
+let startX = 40;
+let drawInterval;
+let rotate = false;
 
 function createFirstPlatform() {
     ctx.fillStyle = "black";
-    let canvasWidth = c.width; 
+    let canvasWidth = c.width;
     ctx.fillRect(0, 400, 50, 200);
 }
 
 function createPlatform() {
     ctx.fillStyle = "black";
-    let canvasWidth = c.width; 
-    let x = Math.random() * (canvasWidth - 50); 
+    let canvasWidth = c.width;
+    let x = Math.random() * (canvasWidth - 50);
     ctx.fillRect(x, 400, 50, 200);
 }
 
 function playerSpawn() {
     ctx.fillStyle = "blue";
-    let canvasWidht = c.widht;
-    
+    let canvasWidth = c.width;
     ctx.fillRect(5, 360, 40, 40);
 }
 
+function clearCanvas() {
+    ctx.clearRect(0, 0, c.width, c.height);
+}
+
 window.addEventListener('mousedown', () => {
+    if (!rotate) {
         isMouseDown = true;
-        ctx.strokeStyle = "blue";
+        ctx.strokeStyle = "red";
         ctx.lineWidth = 2;
         ctx.beginPath();
-        ctx.moveTo(40, 380); // Start point
-        ctx.lineTo(40, startY); // Initial lineTo position
+        ctx.moveTo(50, 380); // start point
+        ctx.lineTo(50, startY); // initial lineto position
         ctx.stroke();
 
         // Start a continuous drawing loop
@@ -37,17 +45,27 @@ window.addEventListener('mousedown', () => {
             if (isMouseDown) {
                 // Continuously increase the lineTo position
                 startY -= 1; // You can adjust the speed by changing the increment
-                ctx.lineTo(40, startY);
+                startX += 1.15;
+                ctx.lineTo(50, startY);
                 ctx.stroke();
             }
-        }, 10); // Adjust the interval for the desired drawing speed
-    });
+        }, 5); // Adjust the interval for the desired drawing speed
+    }
+    else {
+        isMouseDown = true;
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(50, 380); // start point
+        ctx.lineTo(startX, 380); // initial lineto position
+        ctx.stroke();
+    }
+    rotate = !rotate;
+});
 
 window.addEventListener('mouseup', () => {
     isMouseDown = false;
-
-    // Stop the continuous drawing loop
-    clearInterval(drawInterval);
+    clearInterval(drawInterval); // Stop the drawing interval
 });
 
 createFirstPlatform();
